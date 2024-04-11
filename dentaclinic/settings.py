e5beb63fc8a86022d92ca7833fab8305bd339c1f
l0xa1 <l0xa1ch@gmail.com>
2024-04-11 11:07:39 +0500

    compressor

--- dentaclinic/settings.py
+++ dentaclinic/settings.py
@@ -273,7 +273,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "static"
+STATIC_URL = '/static/'
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
